5afc225c7f90f22107de9b520de27006563d3543
Rygel-XVI <micha232@umn.edu>
2019-07-02 20:51:56 -0700

    added code for theater and show models that interacts with the scraper

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
 class Show < ApplicationRecord
   belongs_to :theater
+
+  def self.create_shows_array(show_array)
+    show_array.each {|show| CurtainCallSeattle::Show.new(show) if show != nil}
+  end
+
+  def self.get_shows_by_month(month)
+    self.all.select {|i| i.dates.first.mon <= month && i.dates.last.mon >= month}
+  end
+
+  def self.get_shows_by_name
+    self.all.map {|i| i.name}
+  end
+
+  def self.get_shows_by_date_range(date_array)
+    self.all.select {|show| (show.dates.first <= date_array[1] && show.dates.first >= date_array[0]) || (show.dates.last >= date_array[0] && show.dates.last <= date_array[1]) || (show.dates.first < date_array[0] && show.dates.last > date_array[1])}
+  end
+
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -6,4 +6,16 @@ class Theater < ApplicationRecord
     byebug
   end
 
+  def get_shows_by_name
+    shows.map {|show| show.name}
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|theater| theater.name == name}
+  end
+
+  def get_shows_by_month(month)
+    self.shows.map {|show| array << show if show.show_month_includes?(month)}
+  end
+
 end
